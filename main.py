bd9c1da809daf0ba71aea3677e54bb99aa3f04e5
Paul <paulkaeb@gmx.de>
2025-04-25 16:43:40 +0200

    screen works

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ import os
 # This stops pygame from printing its initialization message
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
+from constants import *
+
 
 def main():
     
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
-
-
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        
+        pygame.display.flip()
+        
 
 
 
