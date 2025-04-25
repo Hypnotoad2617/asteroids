3f7a375493f5fb3600ece2acb6dcc6238b06fbea
Paul <paulkaeb@gmx.de>
2025-04-25 16:10:54 +0200

    initial setup done

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+# This stops pygame from printing its initialization message
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+
+def main():
+    
+    print("Starting Asteroids!")
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
